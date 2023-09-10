d7a907632866639bc77235d7600a45dba3c48752
Cleemzy <razafintsalama.rmh@gmail.com>
2023-09-11 00:11:16 +0300

    add new string function : first_word

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ fn main() {
 
     let text = String::from("textd to work with");
 
-    let index = first_whitespace_index_or_last_index(&text);
+    let first_word = first_word(&text);
 
-    println!("{index}");
-
+    println!("TEXT: {text}|");
+    println!("FIRST WORD OF TEXT: {first_word}|");
 }
 
 // Return the index of the first whitespace in a string, or the last index in case of no whitespace
@@ -20,4 +20,10 @@ fn first_whitespace_index_or_last_index(s: &String) -> usize{
     }
     return s_slice.len(); // return the slice's last index when no whitespace found in loop above
     
+}
+
+// Return the first word of a string
+fn first_word(s: &String) -> String{
+    let i = first_whitespace_index_or_last_index(s); // get the index to get the first word on
+    String::from(&s[..i]) // return the string from the slice at the end of the index
 }
