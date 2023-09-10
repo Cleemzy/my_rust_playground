83237a42e747ed32ea8cffa28f0f48e6b0c66fd9
Cleemzy <razafintsalama.rmh@gmail.com>
2023-09-10 23:46:01 +0300

    add new string function : first_whitespace_index_or_last_index

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
 fn main() {
-    // // First, we declare a type which has `iter` method to get the `Iter` struct (`&[usize]` here):
-    // let slice = &[1, 2, 3];
 
-    // // Then, we iterate over it:
-    // for element in slice.iter() {
-    //     println!("{element}");
-    // }
+    let text = String::from("textd to work with");
 
-    let txt = String::from("Heyy");
-    for c in txt.chars(){
-        println!("{}",c);
-    };
+    let index = first_whitespace_index_or_last_index(&text);
+
+    println!("{index}");
+
+}
+
+// Return the index of the first whitespace in a string, or the last index in case of no whitespace
+fn first_whitespace_index_or_last_index(s: &String) -> usize{
+    let s_slice = s.as_bytes(); // get the bytes slice from the borrowed string 's' in parameter
+
+    // loop over the slice as enumeration 
+    for (i, &c) in s_slice.iter().enumerate() {
+        if c == b' ' {
+            return i; // return the index 'i' when we meet a whitespace
+        }
+    }
+    return s_slice.len(); // return the slice's last index when no whitespace found in loop above
+    
 }
